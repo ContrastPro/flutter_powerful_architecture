62cff9c39d2ef9378a7433ff93e60c062a34fc9e
Gleb Krasnov <glebon0202@gmail.com>
2022-03-18 10:35:03 +0200

    Refactor

--- lib/repositories/auth_repository.dart
+++ lib/repositories/auth_repository.dart
@@ -4,4 +4,27 @@ import 'package:file_structure_flutter/repositories/base_repository.dart';
 class AuthRepository {
   static final BaseRepository _baseRepository = BaseRepository();
   static final Dio _api = _baseRepository.api;
+
+  Future<bool?> signIn({
+    required String phone,
+    required String password,
+  }) async {
+    final FormData formData = FormData();
+
+    formData.fields.addAll([
+      MapEntry('phone', phone),
+      MapEntry('password', password),
+    ]);
+
+    final Response response = await _api.post(
+      'login/',
+      data: formData,
+    );
+
+    if (response.statusCode == 200) {
+      return true;
+    }
+
+    return null;
+  }
 }
